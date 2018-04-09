eb55c5c04d59fddcf11e721fcc7b002568131c74
Luca Bacci <luca.bacci982@gmail.com>
2018-04-09 15:40:36 +0200

    initial implementation of OptionComboBox
    OptionComboBox is a helper class to aid in creation of a simple ComboBox
    consisting only of text, much like Gtk::ComboBoxText, but that can also
    make items be sensitive / insensitive.
    This is for help in porting from Gtk::OptionMenu in gtkmm2.4, a
    combo box that uses a GtkMenu as the underlying model, rather than a
    GtkTreeModel. GtkMenu items can be insensitive and that was used
    extensively in gparted.

---
+++ include/OptionComboBox.h
@@ -0,0 +1,88 @@
+/* Copyright (C) 2018 Luca Bacci
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef GPARTED_OPTIONCOMBOBOX_H
+#define GPARTED_OPTIONCOMBOBOX_H
+
+#include <vector>
+
+/*TODO review gtkmm includes */
+#include <glibmm/ustring.h>
+#include "gtkmm/combobox.h"
+#include "gtkmm/treemodel.h"
+#include "gtkmm/liststore.h"
+
+namespace GParted
+{
+
+class OptionComboBox
+{
+public:
+	class Row
+	{
+	public:
+		Row ( OptionComboBox & parent, Gtk::TreeModel::iterator iter);
+		~Row( );
+
+		void set_text( const Glib::ustring & text );
+		void set_sensitive( bool sensitive );
+
+	private:
+		/*TODO: what about implementing copy?
+		        in that case we should not have a reference to parent,
+		        we should really use a pointer */
+		Row( const Row & );
+		Row & operator=( const Row & );
+	
+		Gtk::TreeModel::iterator m_iter;
+		OptionComboBox& m_parent;
+	};
+
+	OptionComboBox();
+	~OptionComboBox();
+
+	void append(const Glib::ustring & text, bool sensitive = true);
+	std::vector< Row > items();
+	void clear();
+	
+	Glib::ustring get_active_text() const;
+
+private:
+	OptionComboBox( const OptionComboBox & src );              // Not implemented copy constructor
+	OptionComboBox & operator=( const OptionComboBox & rhs );  // Not implemented copy assignment operator
+	
+	class ModelColumns : public Gtk::TreeModel::ColumnRecord
+	{
+	public:
+		ModelColumns() { add(m_col_text); add(m_col_sensitive); }
+
+		Gtk::TreeModelColumn<Glib::ustring> m_col_text;
+		Gtk::TreeModelColumn<bool> m_col_sensitive;
+	private:
+		ModelColumns( const ModelColumns& );
+		ModelColumns & operator=( const ModelColumns & );
+	};
+
+	ModelColumns                   m_columns;
+	Glib::RefPtr<Gtk::ListStore>   m_model;
+	Gtk::CellRendererText          m_cellrenderer;
+	Gtk::ComboBox                  m_combo;
+};
+
+}//GParted
+
+#endif /* GPARTED_COMBOBOX_H */
---
+++ src/OptionComboBox.cc
@@ -0,0 +1,113 @@
+/* Copyright (C) 2018 Luca Bacci
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ComboBox.h"
+
+#include <glibmm/ustring.h>
+
+namespace GParted
+{
+
+OptionComboBox::OptionComboBox()
+{
+	m_model = Gtk::ListStore::create(m_columns);
+	m_combo .set_model( m_model );
+
+	m_combo .pack_start( m_cellrenderer );
+	m_combo .add_attribute( m_cellrenderer, "text",      m_columns.m_col_text );
+	m_combo .add_attribute( m_cellrenderer, "sensitive", m_columns.m_col_sensitive );
+}
+
+OptionComboBox::~OptionComboBox()
+{
+}
+
+Glib::ustring OptionComboBox::get_active_text()
+{
+	Glib::ustring text;
+
+	Gtk::TreeModel::const_iterator iter = m_combo.get_active();
+	if ( iter )
+	{
+		Gtk::TreeModel::Row row = *iter;
+		text = row [ m_columns.m_col_text ];
+	}
+	
+	return text;
+}
+
+void OptionComboBox::append(const Glib::ustring& text, bool sensitive)
+{
+	Gtk::TreeModel::Row row = *( m_model ->append() );
+
+	row [ m_columns.m_col_text ] = text;
+	row [ m_columns.m_col_sensitive ] = sensitive;
+}
+
+std::vector<OptionComboBox::Row> OptionComboBox::items()
+{
+	typedef Gtk::TreeModel::Children type_children;
+
+	std::vector< Row > rows;	
+	type_children children = m_model->children();
+	
+	for (type_children::iterator it = children.begin();
+	     iter != children.end(); ++it)
+	{
+		/*TODO requires c++11 */
+		rows.emplace_back(this, it);
+	}
+	
+	/*TODO only works for c++11 thanks to move semantics if Row is not copyable */
+	return rows;
+}
+
+void OptionComboBox::clear()
+{
+	m_model.clear();
+}
+
+OptionComboBox::Row::Row(OptionComboBox & parent, Gtk::TreeModel::iterator iter)
+ : m_iter(iter),
+   m_parent(parent)
+{
+}
+
+OptionComboBox::Row::~Row()
+{
+}
+
+Glib::ustring OptionComboBox::Row::get_text()
+{
+	return (*iter)[ parent.m_columns.m_col_text ];
+}
+
+void OptionComboBox::Row::set_text(const Glib::ustring& text)
+{
+	(*iter)[ parent.m_columns.m_col_text ] = text;
+}
+
+bool OptionComboBox::Row::get_sensitive()
+{
+	return (*iter)[ parent.m_columns.m_col_sensitive ];
+}
+
+void OptionComboBox::Row::set_sensitive(bool sensitive)
+{
+	(*iter)[ parent.m_columns.m_col_sensitive ] = sensitive;
+}
+
+}//GParted
